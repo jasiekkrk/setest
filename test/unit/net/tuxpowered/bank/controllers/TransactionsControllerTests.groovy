0d0b83162b479933f44ba01b60ff714a0a992c1b
Andrzej Jasinski <jasinski.andrzej@gmail.com>
2012-01-15 19:47:08 +0100

    fixed tests

--- test/unit/net/tuxpowered/bank/controllers/TransactionsControllerTests.groovy
+++ test/unit/net/tuxpowered/bank/controllers/TransactionsControllerTests.groovy
@@ -1,5 +1,6 @@
 package net.tuxpowered.bank.controllers
 
+import net.tuxpowered.bank.services.TransactionsService;
 import grails.test.*
 
 class TransactionsControllerTests extends ControllerUnitTestCase {
@@ -17,7 +18,16 @@ class TransactionsControllerTests extends ControllerUnitTestCase {
     }
 	
 	void testIfRegisterPayment() {
+		//arrange
+		def transactionsServiceMock = mockFor(TransactionsService)
+		transactionsServiceMock.demand.registerPayment{ paramz ->
+			return [errors: [], message: 'Transaction was successful']
+		}
+		controller.transactionsService = transactionsServiceMock.createMock()
+		//act
 		controller.registerPayment()
+		
+		//assert
 		assert "Transaction was successful".equals(controller.flash.message)
 		assert "transactions".equals(controller.redirectArgs.action)
 	}
