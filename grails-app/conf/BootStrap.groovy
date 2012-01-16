5c963f661c82d4dc542420c426775f89be1ead86
Andrzej Jasinski <jasinski.andrzej@gmail.com>
2012-01-16 20:19:07 +0100

    added andrew

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,8 +5,10 @@ class BootStrap {
     def init = { servletContext ->
 		def patsy = new User(email: "patsy@funnybank.com", name: 'Patsy', balance: 200.0 )
 		def joe = new User(email: "joe@funnybank.com", name: 'Joe', balance: 200.0 )
+		def andrew = new User(email: "andrew@funnybank.com", name: 'Adrew', balance: 1.0 )
 		patsy.save(flush:true)
 		joe.save(flush:true)
+		andrew.save(flush:true)
     }
     def destroy = {
     }
